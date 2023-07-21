1c7e7a307a41f9320b213b2cde6f700f346c7edd
BehruzMulkamonov <126500929+BehruzMulkamonov@users.noreply.github.com>
2023-07-21 20:32:26 +0500

    Models and settings is ready

--- API/models.py
+++ API/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+# from phonenumber_field.formfields import PhoneNumberField
 
 # Create your models here.
+
+class Category(models.Model):
+    slug = models.SlugField(max_length=100)
+    name = models.CharField(max_length=120)
+    img = models.ImageField()
+
+
+
+class Product(models.Model):
+    slug = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    price = models.IntegerField()
+    description = models.TextField(max_length=2000)
+    quantity = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
+    
+class Buy(models.Model):
+    quantity = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    phone = models.IntegerField(max_length=12)
+    
--- config/settings.py
+++ config/settings.py
@@ -31,13 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'API',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
