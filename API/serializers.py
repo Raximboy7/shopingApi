210e860954d06e976e486bddd537344e278c1eb2
gitignore.py <126414760+Rustambek571@users.noreply.github.com>
2023-07-21 20:41:17 -0700

    serializers

---
+++ API/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Category, Product, Buy
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('slug', 'name', 'img')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ('slug', 'title', 'price', 'description', 'quantity')
+
+
+class BuySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Buy
+        fields = ('quantity', 'phone', 'product')
