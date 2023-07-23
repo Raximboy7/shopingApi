c6dc2498548bf1ec2592f84f2b8ddd40cf78d07a
asadbek000002 <asadbektuygunov9@gmail.com>
2023-07-23 16:37:39 +0500

    Ustoz kirgazgan kodlarga qoshimchalar qldim va keraksiz kodlar commetga olindi

--- API/urls.py
+++ API/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CategoryViewSet, ProductViewSet, BuyList, BuyViewSet, BuyDetail
+from .views import CategoryViewSet, ProductViewSet, BuyOrder, BuyViewSet
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
-router.register(r'buy', BuyViewSet )
+router.register(r'purchase_history', BuyViewSet )
 
 urlpatterns = [
     path('', include(router.urls)),
-     path('products/<int:pk>/buy/', BuyList.as_view(), name='buy'),
-     path('buy/', BuyViewSet.as_view({'get': 'list'})),
-     path('buy/detail/<int:pk>/', BuyDetail.as_view())
+     path('products/<int:pk>/buy/', BuyOrder.as_view(), name='buy'),
+    #  path('buy/', BuyViewSet.as_view({'get': 'list'})),
+    #  path('buy/detail/<int:pk>/', BuyDetail.as_view())
 ]
